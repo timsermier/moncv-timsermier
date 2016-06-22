3ec51fa5aaec1d66b01af1151d90648f808dbc49
Sermier Tim <tim.sermier@he-arc.ch>
2016-06-22 17:47:52 +0200

    Désinstallation de angular-chart et installation de Chart.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
   'use strict';
   $('a').smoothScroll({speed: 2000});
+  
+  angular.module("app", ["chart.js"]).controller("RadarCtrl", function ($scope) {
+  $scope.labels =["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"];
+
+  $scope.data = [
+    [65, 59, 90, 81, 56, 55, 40],
+    [28, 48, 40, 19, 96, 27, 100]
+  ];
+});
 });
