1e757d51496d990c7bcdaf3074e83bd69366223c
timsermier <tim.sermier@he-arc.ch>
2016-06-23 10:54:27 +0200

    Gestion de l_ouverture des modals lors des clics sur les graphes et gestion des couleurs des graphes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,11 +7,29 @@ $(document).ready(function () {
             var progressValue = parseInt($(obj).find('div').attr('aria-valuenow'));
             var texte = $(obj).find('div').text();
             var idEle = $(obj).find('div').attr('id');
-            var newElement = $('<canvas height="200" width="635px"></canvas>');
-            var backGroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+            var newElement = $('<canvas data-toggle="modal" data-target="#myModal' + idEle + '" height="200" width="635px"></canvas>');
+            var backGroundColor;
+            switch (idEle) {
+                case 'programmation':
+                    backGroundColor = '#5bc0de';
+                    break;
+                case 'reseau':
+                    backGroundColor = '#5cb85c';
+                    break;
+                case 'bdd':
+                    backGroundColor = '#d9534f';
+                    break;
+                case 'virtualisation':
+                    backGroundColor = '#fdc236';
+                    break;
+                case 'autres':
+                    backGroundColor = '#878B00';
+                    break;
+                default:
+                    backGroundColor = 'white';
+            }
             $(obj).replaceWith(newElement);
-            $(newElement).wrap("<div class='col-md-6 col-xs-12 can'></div>");
-
+            $(newElement).wrap('<div class=\'col-md-6 col-xs-12 can\'></div>');
             var myChart = new Chart(newElement, {
                 type: 'doughnut',
                 data: {
@@ -24,16 +42,10 @@ $(document).ready(function () {
                             backgroundColor: [
                                 backGroundColor,
                                 '#FFFFFF'
-                            ],
-                            hoverBackgroundColor: [
-                                backGroundColor
                             ]
                         }]
                 },
                 options: {
-                    tooltipFontSize: 10,
-                    tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %>hrs",
-                    percentageInnerCutout: 100,
                     maintainAspectRatio: false,
                     responsive: true,
                     tooltips: {
