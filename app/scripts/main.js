9d81e00363ee93fc6ab1f921a4ece414f2f1d315
timsermier <tim.sermier@he-arc.ch>
2016-06-23 09:42:20 +0200

    Adaptation visuelle des graphes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,64 +1,47 @@
-$(document).ready(function(){
-  'use strict';
-  $('a').smoothScroll({speed: 2000});
-	/*$("div.progress").replaceWith( "<canvas id=\"myChart\"></canvas>" );	
-  var progressValue[];
-  var label[];
-  for( var i = 0; i < 5; i++ ) {
-    progressValue[i] = parseInt( $("div.progress-bar").attr('aria-valuenow'););
-	label[i] = "test"
-	}
-	
-	var ctx = document.getElementById('myChart');
-	var myChart = new Chart(ctx, {
-		type: 'doughnut',
-		data: {
-		labels: [
-		],
-		datasets: [
-			{
-				data: [progressValue[], 100 - progressValue[]],
-				backgroundColor: [
-					"#FF6384",
-					"#FFFFFF"
-				],
-				hoverBackgroundColor: [
-					"#FF6384",
-					"#FFFFFF"
-				]
-			}]
-	}
-	});*/
-	createChart();
-	function createChart(){
-		$('div.progress').each(function(i, obj) {
-			var progressValue = parseInt($(obj).find("div").attr('aria-valuenow'));
-			var idEle = $(obj).find("div").attr('id');
-			//console.log(idEle);
-			//$(obj).replaceWith( "<canvas id=\"" + idEle + "\"></canvas>" );
-			var newElement = $( "<canvas></canvas>" );
-			$(obj).replaceWith(newElement);
-			var myChart = new Chart(newElement, {
-				type: 'doughnut',
-				data: {
-				labels: [
-					"test", ""
-				],
-				datasets: [
-					{
-						data: [progressValue, 100 - progressValue],
-						backgroundColor: [
-							"#FF6384",
-							"#FFFFFF"
-						],
-						hoverBackgroundColor: [
-							"#FF6384",
-							"#FFFFFF"
-						]
-					}]
-			}
-			});
-		});
-	}
+$(document).ready(function () {
+    'use strict';
+    $('a').smoothScroll({speed: 2000});
+    createChart();
+    function createChart() {
+        $('div.progress').each(function (i, obj) {
+            var progressValue = parseInt($(obj).find('div').attr('aria-valuenow'));
+            var texte = $(obj).find('div').attr('Texte')
+            var idEle = $(obj).find('div').attr('id');
+            var newElement = $('<canvas height="200" width="635px"></canvas>');
+            var backGroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+            $(obj).replaceWith(newElement);
+            $(newElement).wrap("<div class='col-md-6 col-xs-12 can'></div>");
+
+            var myChart = new Chart(newElement, {
+                type: 'doughnut',
+                data: {
+                    labels: [
+                        texte, ''
+                    ],
+                    datasets: [
+                        {
+                            data: [progressValue, 100 - progressValue],
+                            backgroundColor: [
+                                backGroundColor,
+                                '#FFFFFF'
+                            ],
+                            hoverBackgroundColor: [
+                                backGroundColor
+                            ]
+                        }]
+                },
+                options: {
+                    tooltipFontSize: 10,
+                    tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %>hrs",
+                    percentageInnerCutout: 100,
+                    maintainAspectRatio: false,
+                    responsive: true,
+                    tooltips: {
+                        enabled: false
+                    }
+                }
+            });
+        });
+    }
 });
 
